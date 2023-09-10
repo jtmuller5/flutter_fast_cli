32ab24497d35c4715e5a10e08f6308159710c44c
Joseph Muller <jtmuller5@gmail.com>
2023-09-10 18:53:06 -0400

    user

--- flutterfast/lib/app/get_it.config.dart
+++ flutterfast/lib/app/get_it.config.dart
@@ -11,41 +11,46 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i21;
+import 'package:shared_preferences/shared_preferences.dart' as _i23;
 
+import '../features/authentication/services/authentication_service/appwrite_authentication_service.dart'
+    as _i7;
 import '../features/authentication/services/authentication_service/fast_authentication_service.dart'
     as _i5;
 import '../features/authentication/services/authentication_service/firebase_authentication_service.dart'
     as _i6;
 import '../features/authentication/services/authentication_service/supabase_authentication_service.dart'
-    as _i7;
-import '../features/authentication/services/user_service/fast_user_service.dart'
-    as _i18;
-import '../features/authentication/services/user_service/firebase_user_service.dart'
+    as _i8;
+import '../features/authentication/services/user_service/appwrite_user_service.dart'
     as _i20;
-import '../features/authentication/services/user_service/supabase_user_service.dart'
+import '../features/authentication/services/user_service/fast_user_service.dart'
     as _i19;
-import '../features/feedback/services/fast_feedback_service.dart' as _i15;
-import '../features/feedback/services/firebase_feedback_service.dart' as _i16;
-import '../features/feedback/services/supabase_feedback_service.dart' as _i17;
+import '../features/authentication/services/user_service/firebase_user_service.dart'
+    as _i22;
+import '../features/authentication/services/user_service/supabase_user_service.dart'
+    as _i21;
+import '../features/feedback/services/fast_feedback_service.dart' as _i16;
+import '../features/feedback/services/firebase_feedback_service.dart' as _i17;
+import '../features/feedback/services/supabase_feedback_service.dart' as _i18;
 import '../features/monitoring/services/amplitude_analytics_service.dart'
     as _i4;
-import '../features/monitoring/services/crash_service.dart' as _i14;
+import '../features/monitoring/services/crash_service.dart' as _i15;
 import '../features/monitoring/services/fast_analytics_service.dart' as _i3;
-import '../features/monitoring/services/fast_crash_service.dart' as _i13;
-import '../features/settings/services/settings_service.dart' as _i23;
+import '../features/monitoring/services/fast_crash_service.dart' as _i14;
+import '../features/settings/services/settings_service.dart' as _i25;
 import '../features/shared/services/connector_service/fast_connector_service.dart'
-    as _i10;
-import '../features/shared/services/connector_service/firebase_connector_service.dart'
-    as _i12;
-import '../features/shared/services/connector_service/supabase_connector_service.dart'
     as _i11;
-import '../features/shared/services/modules.dart' as _i24;
-import '../features/subscriptions/services/subscription_service.dart' as _i22;
-import '../modules/chat/services/chat_service.dart' as _i9;
-import '../modules/chat/services/fast_chat_service.dart' as _i8;
+import '../features/shared/services/connector_service/firebase_connector_service.dart'
+    as _i13;
+import '../features/shared/services/connector_service/supabase_connector_service.dart'
+    as _i12;
+import '../features/shared/services/modules.dart' as _i26;
+import '../features/subscriptions/services/subscription_service.dart' as _i24;
+import '../modules/chat/services/chat_service.dart' as _i10;
+import '../modules/chat/services/fast_chat_service.dart' as _i9;
 
 const String _firebase = 'firebase';
+const String _appwrite = 'appwrite';
 const String _supabase = 'supabase';
 
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -66,42 +71,50 @@ Future<_i1.GetIt> $initGetIt(
     registerFor: {_firebase},
   );
   gh.singleton<_i5.FastAuthenticationService>(
-    _i7.SupabaseAuthenticationService(),
+    _i7.AppwriteAuthenticationService(),
+    registerFor: {_appwrite},
+  );
+  gh.singleton<_i5.FastAuthenticationService>(
+    _i8.SupabaseAuthenticationService(),
     registerFor: {_supabase},
   );
-  gh.lazySingleton<_i8.FastChatService>(() => _i9.ChatService());
-  gh.factory<_i10.FastConnectorService>(
-    () => _i11.SupabaseConnectorService(),
+  gh.lazySingleton<_i9.FastChatService>(() => _i10.ChatService());
+  gh.factory<_i11.FastConnectorService>(
+    () => _i12.SupabaseConnectorService(),
     registerFor: {_supabase},
   );
-  gh.factory<_i10.FastConnectorService>(
-    () => _i12.FirebaseConnectorService(),
+  gh.factory<_i11.FastConnectorService>(
+    () => _i13.FirebaseConnectorService(),
     registerFor: {_firebase},
   );
-  gh.singleton<_i13.FastCrashService>(_i14.CrashService());
-  gh.lazySingleton<_i15.FastFeedbackService>(
-    () => _i16.FirebaseFeedbackService(),
+  gh.singleton<_i14.FastCrashService>(_i15.CrashService());
+  gh.lazySingleton<_i16.FastFeedbackService>(
+    () => _i17.FirebaseFeedbackService(),
     registerFor: {_firebase},
   );
-  gh.lazySingleton<_i15.FastFeedbackService>(
-    () => _i17.SupabaseFeedbackService(),
+  gh.lazySingleton<_i16.FastFeedbackService>(
+    () => _i18.SupabaseFeedbackService(),
     registerFor: {_supabase},
   );
-  gh.lazySingleton<_i18.FastUserService>(
-    () => _i19.SupabaseUserService(),
+  gh.lazySingleton<_i19.FastUserService>(
+    () => _i20.AppwriteUserservice(),
+    registerFor: {_appwrite},
+  );
+  gh.lazySingleton<_i19.FastUserService>(
+    () => _i21.SupabaseUserService(),
     registerFor: {_supabase},
   );
-  gh.lazySingleton<_i18.FastUserService>(
-    () => _i20.FirebaseUserService(),
+  gh.lazySingleton<_i19.FastUserService>(
+    () => _i22.FirebaseUserService(),
     registerFor: {_firebase},
   );
-  await gh.factoryAsync<_i21.SharedPreferences>(
+  await gh.factoryAsync<_i23.SharedPreferences>(
     () => registerModule.sharedPrefs,
     preResolve: true,
   );
-  gh.singleton<_i22.SubscriptionService>(_i22.SubscriptionService());
-  gh.singleton<_i23.SettingsService>(_i23.SettingsService());
+  gh.singleton<_i24.SubscriptionService>(_i24.SubscriptionService());
+  gh.singleton<_i25.SettingsService>(_i25.SettingsService());
   return getIt;
 }
 
-class _$RegisterModule extends _i24.RegisterModule {}
+class _$RegisterModule extends _i26.RegisterModule {}
--- flutterfast/lib/features/authentication/services/user_service/appwrite_user_service.dart
+++ flutterfast/lib/features/authentication/services/user_service/appwrite_user_service.dart
@@ -1,22 +1,55 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutterfast/app/get_it.dart';
+import 'package:flutterfast/app/services.dart';
 import 'package:flutterfast/features/authentication/models/fast_user.dart';
 import 'package:flutterfast/features/authentication/services/user_service/fast_user_service.dart';
+import 'package:injectable/injectable.dart';
 
+@appwrite
+@LazySingleton(as: FastUserService)
 class AppwriteUserservice extends FastUserService {
-  @override
-  Future<void> createUser() {
+  final client = Client().setEndpoint('https://cloud.appwrite.io/v1').setProject(const String.fromEnvironment('APPWRITE_PROJECT_ID'));
 
+  Databases get databases => Databases(client);
+
+  @override
+  Future<void> createUser() async {
+    try {
+      String? id = authenticationService.id;
+
+      if (id != null) {
+        await databases.createDocument(
+          databaseId: 'general',
+          collectionId: 'users',
+          documentId: id,
+          data: FastUser(
+            id: id,
+            createdAt: DateTime.now(),
+          ).toJson(),
+        );
+      }
+    } on AppwriteException catch (e) {
+      debugPrint(e.message);
+    }
   }
 
   @override
-  Future<void> deleteUser(FastUser user) {
-    // TODO: implement deleteUser
-    throw UnimplementedError();
+  Future<void> deleteUser(FastUser user) async {
+    await databases.deleteDocument(
+      databaseId: 'general',
+      collectionId: 'users',
+      documentId: user.id,
+    );
   }
 
   @override
-  Future<void> updateUser(FastUser user) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<void> updateUser(FastUser user) async{
+    await databases.updateDocument(
+      databaseId: 'general',
+      collectionId: 'users',
+      documentId: user.id,
+      data: user.toJson(),
+    );
   }
-
 }
