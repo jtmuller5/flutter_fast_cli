b88e7123d2aca8d7b14ea95c9cc48a940ba98fd3
Joseph Muller <jtmuller5@gmail.com>
2024-02-21 10:18:22 -0500

    Breakpoints

--- flutterfast/lib/app/constants.dart
+++ flutterfast/lib/app/constants.dart
@@ -42,10 +42,35 @@ extension SliverExtension on Widget {
   Widget sliver() => SliverToBoxAdapter(child: this);
 }
 
+// https://m2.material.io/design/layout/responsive-layout-grid.html#breakpoints
 class Breakpoints {
-  static const double xs = 480.0;
-  static const double sm = 768.0;
-  static const double md = 992.0;
-  static const double lg = 1200.0;
-  static const double xl = 1400.0;
+  static const double xs = 600.0; // Extra-small (phone)
+  static const double sm = 905.0; // Small (tablet)
+  static const double md = 1240.0; // Medium (laptop)
+  static const double lg = 1440.0; // Large (desktop)
+}
+
+extension Margins on BuildContext {
+  double get margin {
+    final dpr = MediaQuery.of(this).devicePixelRatio;
+    final width = MediaQuery.of(this).size.width / dpr;
+    if (width < Breakpoints.xs) {
+      return 16;
+    } else if (width < Breakpoints.sm) {
+      return 32;
+    } else if (width < Breakpoints.md) {
+      return 32;
+    } else if (width < Breakpoints.lg) {
+      return 200;
+    } else {
+      return 0;
+    }
+  }
+
+  Widget marginBox(Widget child) {
+    return Padding(
+      padding: EdgeInsets.all(margin),
+      child: child,
+    );
+  }
 }
