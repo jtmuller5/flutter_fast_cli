c07bfe7383aaf9118f8538a3d0e498ab656fe5d4
Joseph Muller <jtmuller5@gmail.com>
2024-05-05 17:46:22 -0400

    Implement chat for pocketbase

--- flutterfast/lib/modules/chat/services/pocketbase_chat_service.dart
+++ flutterfast/lib/modules/chat/services/pocketbase_chat_service.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutterfast/app/get_it.dart';
+import 'package:flutterfast/features/authentication/services/authentication_service/pocketbase_authentication_service.dart';
 import 'package:flutterfast/modules/chat/models/fast_message.dart';
 import 'package:flutterfast/modules/chat/services/fast_chat_service.dart';
 import 'package:injectable/injectable.dart';
@@ -7,14 +9,23 @@ import 'package:injectable/injectable.dart';
 @LazySingleton(as: FastChatService)
 class PocketbaseChatService extends FastChatService {
   @override
-  Future<void> getMessages() {
-    // TODO: implement getMessages
-    throw UnimplementedError();
+  Future<void> getMessages() async {
+    final messagesResponse = await pb.collection('messages').getList();
+
+    final messages = messagesResponse.items.map((e) => FastMessage.fromJson(e.data)).toList();
+    setMessages(messages);
   }
 
   @override
-  Future<void> submitMessage(FastMessage message) {
-    // TODO: implement submitMessage
-    throw UnimplementedError();
+  Future<void> submitMessage(FastMessage message) async {
+    try {
+      await pb.collection('messages').create(body: message.toJson());
+      setMessages([
+        message,
+        ...messages.value,
+      ]);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
